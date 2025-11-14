1db5153ff7c8a60370c3371af2ce96145a863a16
Meezyr <corentin.d@polarion.fr>
2025-11-14 18:58:54 +0100

    [Fix] Remove Customizer JS file

--- inc/customizer.php
+++ inc/customizer.php
@@ -51,11 +51,3 @@ function template_wordpress_customize_partial_blogname() {
 function template_wordpress_customize_partial_blogdescription() {
 	bloginfo( 'description' );
 }
-
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
-function template_wordpress_customize_preview_js() {
-	wp_enqueue_script( 'template-wordpress-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
-}
-add_action( 'customize_preview_init', 'template_wordpress_customize_preview_js' );
